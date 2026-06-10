455ab08bf53ad57da324d188a411b6dfd7c099de
Luciano Soto <lucianosotobonja95@gmail.com>
2026-06-10 19:22:09 -0300

    feat: add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,12 @@ import cors from "cors";
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
-
 app.use(cors({
   origin: "http://localhost:3000",
   methods: ["GET", "POST", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
+app.use(express.json());
 
 routerApi(app);
 app.use(errorHandler);
